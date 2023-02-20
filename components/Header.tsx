c6dd73c2edd529c676b260a67b770988438d7a21
youknowme <ys1113457623@gmail.com>
2023-02-20 13:36:00 +0530

    Header skeleton Done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {}
 
 export default function Header({}: Props) {
-  return (
-    <header>
-        <div>
-            {/* Logo */}
-        </div>
+  return ( 
+    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto'>
+      <div className='flex flex-row items-center'>
+        {/* Logo */}
+        <SocialIcon
+          url="https://www.twitter.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <SocialIcon
+          url="https://www.instagram.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+      </div>
     </header>
-  )
+  );
 }
