66cf7d679e3add236a4e8116d49eefe5a7258917
Sahil Sharma <sahil.sharma@codibex.com>
2023-03-03 21:07:18 +0530

    Git Commit

---
+++ schemas/skill.js
@@ -0,0 +1,33 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'skill',
+  title: 'Skill',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'The title of the project',
+      type: 'string',
+    },
+    {
+      name: 'progress',
+      title: 'Progress',
+      type: 'number',
+      description: 'The progres of the skill',
+      validation: (Rule) => Rule.required().min(0).max(100),
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+})
